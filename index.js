1bcd0e7eae342cd41d82a3ed8f041b14b6280437
Moises Cruz <moiscye@gmail.com>
2019-07-30 15:25:09 +1000

    change greeting

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
-  res.send({ name: "First Name", phone: "1234566789" });
+  res.send({
+    name: "First Name",
+    phone: "1234566789",
+    lastName: "Second Name"
+  });
 });
 
 app.listen(PORT, () => console.log(`listening in port ${PORT}`));
