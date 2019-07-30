6c93175e95b11395152e097da626609303405265
Moises Cruz <moiscye@gmail.com>
2019-07-30 15:13:39 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send({ name: "First Name", phone: "1234566789" });
+});
+
+app.listen(PORT, () => console.log(`listening in port ${PORT}`));
