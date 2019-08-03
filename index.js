7b290f7b23b978353cd66fbe2087dbbc37b986b7
Moises Cruz <moiscye@gmail.com>
2019-08-03 20:13:21 +1000

    Fix. Redirecting from express for unknown routes

--- index.js
+++ index.js
@@ -42,6 +42,7 @@ if (process.env.NODE_ENV === "production") {
 
   //Express will serve up the index.html
   //if it does not recognize the route like /surveys
+  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
